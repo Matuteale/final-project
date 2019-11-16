eb6befb57e67615e68ac9db267ceec93dcf0f41b
matuteale <mbuscagl@itba.edu.ar>
2019-11-16 11:48:57 -0300

    Fix raw data writer

--- raw_data_writer.py
+++ raw_data_writer.py
@@ -4,8 +4,9 @@ import threading
 import sys, select
 import thread
 import time, datetime
+import math
 
-def startFileWriter(headset, start_time, file_id, video_ready, collector_max_millis):
+def startFileWriter(headset, start_time, file_id, video_ready, collector_max_seconds):
   try:
     filename = './data/raw/data_' + file_id + '_' + start_time + '.csv'
     f = open(filename, 'w')
@@ -13,18 +14,20 @@ def startFileWriter(headset, start_time, file_id, video_ready, collector_max_mil
     video_ready.acquire()
     video_ready.wait()
 
-    initial_time = int(time.time()*1000)
+    initial_time = int(time.time())
+    time_sleeped = 0
       
-    while (int(time.time()*1000) < initial_time + collector_max_millis):
+    while int(time.time()) < initial_time + collector_max_seconds - int(math.ceil(time_sleeped)):
       input = select.select([sys.stdin], [], [], 0)[0]
       if input:
         print('Exiting raw data writer...')
         break
       
       time.sleep(.01)
-      (count, eeg) = (headset.count, headset.raw_value)
+      time_sleeped += 0.01
+      (count, eeg, blink) = (headset.count, headset.raw_value, headset.blink)
 
       data_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S.%f')
-      f.write(str(data_time) + ',' + str(eeg) + ',' + '\n')
+      f.write(str(data_time) + ',' + str(eeg) + ',' + str(blink) + '\n')
   finally:
     f.close()
